d4b616ad549dd0d40a0e84b0af257f499d02da42
Wout <woutferinga@live.nl>
2019-09-17 10:44:31 +0200

    boggle final version

--- week_1/river_crossing.py
+++ week_1/river_crossing.py
@@ -3,17 +3,18 @@ start_state = "FGCW|"
 end_state = "|FGCW"
 left_state = ""
 right_state = ""
-invalid_states = ("GW", "CG", "WG", "GW")
-
+invalid_states = ("GW", "CG", "WG", "GW", "GCW", "CGW", "CWG", "WCG", "GWC")
+previous_state = ""
 moves = []
 
 def move_left(move): 
     left_state = move
     #if is_safe(left_state):  
     possible_moves = next_moves(left_state)
+    valid_moves = next_valid_moves(possible_moves, left_state)
     for move in possible_moves: 
         moves.append(move)
-        move_right(move)
+        # move_right(move)
     
 
 def move_right(move):
@@ -23,7 +24,6 @@ def move_right(move):
         moves.append(move)
         move_left(move)
     return 
-
 
 # helper function for checking if given state is valid
 def is_safe(side_state, move):
@@ -43,15 +43,27 @@ def reached_end_state(state):
 
 def next_moves(state):
     next_moves = ["F"]
-    state.remove("F")
+    state.replace("F", "")
     if state != "": 
         for prop in state:
-            if prop != "|":
+            if prop != "|" and prop != "F":
                 next_moves.append("F" + prop)
-
+        print(next_moves)
         return next_moves
     else: 
+        print(next_moves)
         return next_moves
-        
+
+def next_valid_moves(possible_moves, left_state):
+    valid_moves = []
+    state = left_state.replace("|", "")
+    print(state)
+    #check of wanner wij de possible_move uitvoeren de state die hieruit voortkomt nog een valide state is. zo ja, voeg hem toe aan valid_moves
+    for prop in possible_moves:
+        if prop not in invalid_states:
+            print("adding valid move : " + prop)
+            valid_moves.append(prop)
+    return valid_moves
 
 # start state begins on leftside
+move_left(start_state)
