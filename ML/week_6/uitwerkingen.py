175ea4e4073650d4aef0aee7d9d544f8e660c7c1
Wouter Manuel <349618d.W.m!>
2019-11-05 14:58:13 +0100

    opdrachten week 6 tot en met 2

--- ML/week_6/uitwerkingen.py
+++ ML/week_6/uitwerkingen.py
@@ -9,10 +9,9 @@ def plotImage(img, label):
     # plaatje dat in de matrix is weergegeven. Zorg ervoor dat dit grafisch wordt weergegeven.
     # Maak gebruik van plt.cm.binary voor de cmap-parameter van plt.imgshow.
 
-    # YOUR CODE HERE
-
-    pass
-
+    plt.xlabel(label)
+    plt.imshow(img, cmap=plt.cm.binary)
+    plt.show()
 
 # OPGAVE 1b
 def scaleData(X):
@@ -21,24 +20,30 @@ def scaleData(X):
     # alle maximal waarde die in de matrix voorkomt.
     # Deel alle elementen in de matrix 'element wise' door de grootste waarde in deze matrix.
 
-    # YOUR CODE HERE
-
-    pass
+    return X/np.amax(X)
 
 # OPGAVE 1c
 def buildModel():
     # Deze methode maakt het keras-model dat we gebruiken voor de classificatie van de mnist
     # dataset. Je hoeft deze niet abstract te maken, dus je kunt er van uitgaan dat de input
-    # layer van dit netwert alleen geschikt is voor de plaatjes in de opgave (wat is de 
+    # layer van dit netwerk alleen geschikt is voor de plaatjes in de opgave (wat is de 
     # dimensionaliteit hiervan?).
     # Maak een model met een input-laag, een volledig verbonden verborgen laag en een softmax
     # output-laag. Compileer het netwerk vervolgens met de gegevens die in opgave gegeven zijn
     # en retourneer het resultaat.
 
-    model = None
+    model = keras.Sequential()
+    # input laag
+    model.add(keras.layers.Flatten(input_shape=(28,28)))
 
-    # YOUR CODE HERE
+    # hidden layer
+    model.add(keras.layers.Dense(128, activation='relu'))
 
+    # output layer
+    model.add(keras.layers.Dense(10, activation='softmax')) 
+    
+    # tijd om te compileren 
+    model.compile(loss=keras.losses.sparse_categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
     return model
 
 
@@ -46,9 +51,8 @@ def buildModel():
 def confMatrix(labels, pred):
     # Retourneer de econfusion matrix op basis van de gegeven voorspelling (pred) en de actuele
     # waarden (labels). Check de documentatie van tf.math.confusion_matrix
-    
-    # YOUR CODE HERE
-    
+
+    return tf.math.confusion_matrix(labels, pred, num_classes=10)
 
 # OPGAVE 2b
 def confEls(conf, labels): 
@@ -57,12 +61,23 @@ def confEls(conf, labels):
     # waarden van de TP, FP, FN en TN conform de berekening in de opgave. Maak vervolgens gebruik van
     # de methodes zip() en list() om een list van len(labels) te retourneren, waarbij elke tupel 
     # als volgt is gedefinieerd:
-
     #     (categorie:string, tp:int, fp:int, fn:int, tn:int)
- 
     # Check de documentatie van numpy diagonal om de eerste waarde te bepalen.
- 
-    # YOUR CODE HERE
+    
+    resultList = list()
+
+    # alle true positives in de confusion matrix 
+    tp = np.diagonal(conf)
+
+    for i in range(len(labels)):
+        tp_i = tp[i]
+        fp_i = sum(conf[:, i].numpy() - tp_i)
+        fn_i = sum(conf[i, :].numpy() - tp_i)
+        tn_i = sum(sum(conf.numpy() -  tp_i - fp_i - fn_i))
+
+        resultList.append((labels[i], tp_i, fp_i, fn_i, tn_i))
+    
+    return resultList 
 
 # OPGAVE 2c
 def confData(metrics):
@@ -73,13 +88,29 @@ def confData(metrics):
 
     # VERVANG ONDERSTAANDE REGELS MET JE EIGEN CODE
     
-    tp = 1
-    fp = 1
-    fn = 1
-    tn = 1
+    tp = sum([n[1] for n in metrics])
+    fp = sum([n[2] for n in metrics])
+    fn = sum([n[3] for n in metrics])
+    tn = sum([n[4] for n in metrics])
+
+    print(tp, fp, fn, tn)
 
     # BEREKEN HIERONDER DE JUISTE METRIEKEN EN RETOURNEER DIE 
     # ALS EEN DICTIONARY
-
     rv = {'tpr':0, 'ppv':0, 'tnr':0, 'fpr':0 }
+
+    # true positive rate : zegt iets over de verhouding tussen correct geclassificeerde kledingstukken
+    # en het totaal aantal kledingstukken dat tot die klasse behoren
+    rv['tpr'] = tp/(tp+fn)
+    
+    # positive predictive value: hoeveel van de voorspelde kledingstukken ook daadwerkelijk 
+    # hetgene is wat het model beweert
+    rv['ppv'] = tp/(tp+fp)
+
+    # true negative rate: verhouding tussen foutief geclassificeerde kledingstukken van het totaal
+    rv['tnr'] = tn/(tn+fp)
+
+    # false positive rate: verhouding tussen positief geclassificeerde kledingstukken van het totaal
+    rv['fpr'] = fp/(fp+tn)
+
     return rv
